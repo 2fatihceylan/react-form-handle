2e8f68734df88957fc44da97b6113f1eb3253905
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2024-01-10 19:30:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+
+
+  const [form, setForm] = useState(
+    {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      address: ''
+    }
+  );
+
+  const handleFormChange = (event) =>{
+
+    setForm((prev)=>(
+      {
+        ...prev,
+        [event.target.name]: event.target.value,
+      }
+    ));
+  };
+
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(form)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='row'>
+        
+        <input
+          type='text'
+          onChange={handleFormChange}
+          name='firstname'
+          placeholder='First Name'
+        />
+        <input
+          type='text'
+          onChange={handleFormChange}
+          name='lastname'
+          placeholder='Last Name'
+        />
+        <input
+          type='email'
+          onChange={handleFormChange}
+          name='email'
+          placeholder='Email'
+        />
+        <input
+          type='password'
+          onChange={handleFormChange}
+          name='password'
+          placeholder='Password'
+        />
+        <input
+          type='text'
+          onChange={handleFormChange}
+          name='address'
+          placeholder='Address'
+        />
+
+        <Button onClick={submit} variant="success">Submit</Button>
+
+          
+      </form>
+     
     </div>
   );
 }
